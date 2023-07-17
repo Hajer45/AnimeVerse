ea93c1873c27a971488cdfab5d733931a5280a33
unknown <hagartalbi@gmail.com>
2023-07-17 09:07:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import Auth from './components/Auth/Auth'
+
+function App() {
+
+  return (
+    <>
+      <Auth/>
+      <h1></h1>
+    </>
+  )
+}
+
+export default App
